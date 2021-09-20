5ca524f4d3d171c33b8d292b59fbc1246eb60db9
muhammad-muchson <muchson@alterra.id>
2021-09-20 10:37:43 +0700

    passangerInput
    mengganti dari class component ke funciton hook

--- src/component/PassengerInput.js
+++ src/component/PassengerInput.js
@@ -1,100 +1,93 @@
-import { Component } from "react";
-import "./Home.css";
+import { useState } from "react"
+import "./Home.css"
 
-class PassengerInput extends Component {
-      state = {
-        nama: "",
-        umur: "",
-        jenisKelamin: "Pria",
-        editing: true
-      }
-      onChange = e => {
-        this.setState({
-          [e.target.name]: e.target.value,    
+function PassengerInput(props) {
+  const [state, setState] = useState({
+    nama: "",
+    umur: "",
+    jenisKelamin: "Pria",
+    editing: true,
+  })
+
+  const onChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    if (state.nama.trim() && state.umur && state.jenisKelamin) {
+      const umur = state.umur
+      if (umur >= 75 || umur <= 12) {
+        alert("Umur tidak sesuai")
+      } else {
+        const newData = {
+          nama: state.nama,
+          umur: state.umur,
+          jenisKelamin: state.jenisKelamin,
+        }
+        props.tambahPengunjung(newData)
+        setState({
+          ...state,
+          nama: "",
+          umur: "",
+          jenisKelamin: "Pria",
         })
       }
-    
-      handleSubmit = e => {
-        e.preventDefault();
-        if(this.state.nama.trim() && this.state.umur && this.state.jenisKelamin) {
-          const umur = this.state.umur
-          if (umur >= 75 || umur <= 12) {
-            alert("Umur tidak sesuai")
-          } else {
-            const newData = {
-              nama: this.state.nama,
-              umur: this.state.umur,
-              jenisKelamin: this.state.jenisKelamin
-            }
-            this.props.tambahPengunjung(newData);
-            this.setState({    
-              nama: "",
-              umur: "",
-              jenisKelamin: "Pria"
-            });
-          }
-        } else {
-          alert("Data masih ada yang kosong")
-        } 
-      };
+    } else {
+      alert("Data masih ada yang kosong")
+    }
+  }
 
-      handleBukaInput = () => {
-        this.setState({
-            editing: false
-          })
-      }
+  const handleBukaInput = () => {
+    setState({
+      ...state,
+      editing: false,
+    })
+  }
 
-      handleTutupInput = () => {
-        this.setState({
-            editing: true
-          })
-      }
-    
-      render() {
+  const handleTutupInput = () => {
+    setState({
+      ...state,
+      editing: true,
+    })
+  }
 
-        let viewMode = {};
-        let editMode = {};
+  let viewMode = {}
+  let editMode = {}
 
-        if (this.state.editing) {
-            viewMode.display = 'none';
-        } else {
-            editMode.display = 'none';
-        }
+  if (state.editing) {
+    viewMode.display = "none"
+  } else {
+    editMode.display = "none"
+  }
 
-        return (
-            <div>
-                <div onSubmit={this.handleSubmit} style={viewMode}>
-                    <p>Masukkan Nama Anda</p>
-                    <input
-                        type="text"
-                        className="input-text"
-                        placeholder="Nama anda ..."
-                        value={this.state.nama}
-                        name="nama"          
-                        onChange={this.onChange}
-                    />
-                    <p>Masukkan Umur Anda</p>
-                    <input
-                        type="number"
-                        className="input-text"
-                        placeholder="Umur anda ..."
-                        value={this.state.umur}
-                        name="umur"          
-                        onChange={this.onChange}
-                    />
-                    <p>Masukkan Jenis Kelamin Anda</p>
-                    <select onChange={this.onChange} name="jenisKelamin">
-                      <option value="Pria" selected>Pria</option>
-                      <option value="Wanita">Wanita</option>
-                    </select>
-                    <p></p>
-                    <button onClick={this.handleSubmit}>Submit</button>
-                    <button onClick={this.handleTutupInput} style={{marginLeft: "10px"}}>Selesai</button>
-                </div>
-                <button className="inputan" onClick={this.handleBukaInput} style={editMode}>Masukkan Nama Pelanggan</button>
-            </div>
-        )
-      }
+  return (
+    <div>
+      <div onSubmit={handleSubmit} style={viewMode}>
+        <p>Masukkan Nama Anda</p>
+        <input type="text" className="input-text" placeholder="Nama anda ..." value={state.nama} name="nama" onChange={onChange} />
+        <p>Masukkan Umur Anda</p>
+        <input type="number" className="input-text" placeholder="Umur anda ..." value={state.umur} name="umur" onChange={onChange} />
+        <p>Masukkan Jenis Kelamin Anda</p>
+        <select onChange={onChange} name="jenisKelamin">
+          <option value="Pria" selected>
+            Pria
+          </option>
+          <option value="Wanita">Wanita</option>
+        </select>
+        <p></p>
+        <button onClick={handleSubmit}>Submit</button>
+        <button onClick={handleTutupInput} style={{ marginLeft: "10px" }}>
+          Selesai
+        </button>
+      </div>
+      <button className="inputan" onClick={handleBukaInput} style={editMode}>
+        Masukkan Nama Pelanggan
+      </button>
+    </div>
+  )
 }
 
-export default PassengerInput;
+export default PassengerInput
